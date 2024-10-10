b9f604544a6437de782c325a24171b21f6235a3d
Task View <maumauriciog@hotmail.com>
2024-10-10 16:47:27 -0300

    change the icon, v2

--- app/src/main/java/com/devspacecomposeinit/MainActivity.kt
+++ app/src/main/java/com/devspacecomposeinit/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -42,13 +45,47 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val newArtist = Artist(
-                        name = "Alfred Sisley",
+                    val leonardo = Artist(
+                        name = "Leonardo da Vinci",
                         lastSeenOnLine = "3days ago",
                         image = R.drawable.ic_leonardo_da_vinci,
                         cards = R.drawable.ic_mona_lisa
                     )
-                    ArtistCard(newArtist)
+                    val pablo = Artist(
+                        name = "Pablo Picasso",
+                        lastSeenOnLine = "6days ago",
+                        image = R.drawable.ic_pablo_picasso,
+                        cards = R.drawable.ic_beijo
+                    )
+                    val salvador = Artist(
+                        name = "Salvador Dali",
+                        lastSeenOnLine = "8days ago",
+                        image = R.drawable.ic_salvador_dali,
+                        cards = R.drawable.ic_persistence_of_memory
+                    )
+
+                    val vicente = Artist(
+                        name = "Vicent Vangovi",
+                        lastSeenOnLine = "18days ago",
+                        image = R.drawable.ic_vincent_van_gogh,
+                        cards = R.drawable.ic_starry_night
+                    )
+
+                    val artists = listOf(
+                        salvador, vicente,
+                        leonardo, salvador, pablo, vicente, leonardo, salvador
+                    )
+
+                    LazyColumn {
+                        items(artists) { artist ->
+                            ArtistCard(artist,
+                                onClick = {
+                                    println("You clicked on the artist name ${artist.name}..")
+                                }
+                            )
+                        }
+                    }
+
                 }
             }
         }
@@ -56,9 +93,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ArtistCard(artist: Artist) {
+fun ArtistCard(
+    artist: Artist,
+    onClick: () -> Unit
+) {
     Column(
-        modifier = Modifier.padding(8.dp)
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable(onClick = onClick)
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             Image(
@@ -88,7 +130,8 @@ fun ArtistCard(artist: Artist) {
             elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
         ) {
             Image(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .height(235.dp),
                 contentScale = ContentScale.Crop,
                 painter = painterResource(id = artist.cards),
@@ -116,6 +159,11 @@ fun ArtistCardPreview() {
             image = R.drawable.ic_leonardo_da_vinci,
             cards = R.drawable.ic_mona_lisa
         )
-        ArtistCard(newArtist)
+        ArtistCard(
+            newArtist,
+            onClick = {
+                println("")
+            }
+        )
     }
 }
